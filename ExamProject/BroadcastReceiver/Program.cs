2294c4d2ab921d236fdb7df57ee8fa3fbf2dc5fb
Morten Toudahl <toudahl@protonmail.ch>
2015-11-23 14:13:01 +0100

    Added code to run the broadcaster
    Minor modification to Web.config

--- ExamProject/BroadcastReceiver/Program.cs
+++ ExamProject/BroadcastReceiver/Program.cs
@@ -10,6 +10,17 @@ namespace BroadcastReceiver
     {
         static void Main(string[] args)
         {
+            Task.Run(() => new UdpBroadcastReceiver(new TeacherLoungeBroadcast()));
+            Console.WriteLine("Press any key to stop listening for UDP broadcasts");
+            Console.ReadKey();
+        }
+    }
+
+    internal class TeacherLoungeBroadcast : IBroadcastInterpreter
+    {
+        public void HandleBroadcast(byte[] input)
+        {
+            
         }
     }
 }
