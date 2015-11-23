7ec5532d529034e912ef65c3170facb6243d249a
Morten Toudahl <toudahl@protonmail.ch>
2015-11-23 15:07:07 +0100

    Removed remnants of broadcastreceiver
    The SensorReader now takes an integer as an argument, when the executable is run.
    It will use the integer as port number, within the allowed port range

--- ExamProject/SensorReader/Program.cs
+++ ExamProject/SensorReader/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,19 +12,57 @@ namespace SensorReader
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Trace.WriteLine("Write the port you want to listen on.");
+                return;
+            }
 
-            var receiver = new UdpBroadcastReceiver(new TeacherLoungeBroadcast());
-            Task.Run(() => receiver.ListenForBroadcast(7000));
+            SetupTracing();
+
+            int port = ParseArgsToInt(args[0]);
+
+            if (!(port >= 0 && port <= 65535))
+            {
+                Trace.TraceWarning("Ports must be in the range 0-65535");
+                return;
+            }
+
+            StartReciever(port);
             Console.WriteLine("Press any key to stop listening for UDP broadcasts");
             Console.ReadKey();
         }
-    }
 
-    internal class TeacherLoungeBroadcast : IBroadcastInterpreter
-    {
-        public void HandleBroadcast(byte[] input)
+        private static void StartReciever(int port)
         {
+            var receiver = new UdpBroadcastReceiver(new TeacherLoungeBroadcast());
+            Task.Run(() => receiver.ListenForBroadcast(port));
+        }
 
+        private static int ParseArgsToInt(string args)
+        {
+            try
+            {
+                return Convert.ToInt32(args);
+            }
+            catch(FormatException)
+            {
+                Trace.TraceInformation("input port number as integer. ie, 7000");
+            }
+            catch(Exception)
+            {
+                Trace.TraceWarning("Unexpected exception");
+            }
+            return -1;
+        }
+
+        private static void SetupTracing()
+        {
+            Trace.AutoFlush = true;
+            Trace.Listeners.Add(new ConsoleTraceListener
+                                {
+                                    Filter = new EventTypeFilter(SourceLevels.All)
+                                });
         }
     }
 }
