13977d3d19a3d94283605dcf2128ecc2aa7f1b01
peter frank <peto.frank@gmail.com>
2015-11-23 14:04:09 +0100

    Db connect
    Created controllers
    sensor
    data_values

---
+++ ExamProject/WebApi/Controllers/sensorsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApi;
+
+namespace WebApi.Controllers
+{
+    public class sensorsController : ApiController
+    {
+        private Model1 db = new Model1();
+
+        // GET: api/sensors
+        public IQueryable<sensor> Getsensors()
+        {
+            return db.sensors;
+        }
+
+        // GET: api/sensors/5
+        [ResponseType(typeof(sensor))]
+        public IHttpActionResult Getsensor(int id)
+        {
+            sensor sensor = db.sensors.Find(id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sensor);
+        }
+
+        // PUT: api/sensors/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putsensor(int id, sensor sensor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != sensor.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(sensor).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!sensorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/sensors
+        [ResponseType(typeof(sensor))]
+        public IHttpActionResult Postsensor(sensor sensor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.sensors.Add(sensor);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = sensor.id }, sensor);
+        }
+
+        // DELETE: api/sensors/5
+        [ResponseType(typeof(sensor))]
+        public IHttpActionResult Deletesensor(int id)
+        {
+            sensor sensor = db.sensors.Find(id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+
+            db.sensors.Remove(sensor);
+            db.SaveChanges();
+
+            return Ok(sensor);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool sensorExists(int id)
+        {
+            return db.sensors.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ ExamProject/WebApi/data_values.cs
@@ -0,0 +1,22 @@
+namespace WebApi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class data_values
+    {
+        public int id { get; set; }
+
+        [StringLength(50)]
+        public string value { get; set; }
+
+        public DateTime? created_on { get; set; }
+
+        public int sensor_id { get; set; }
+
+        public virtual sensor sensor { get; set; }
+    }
+}
