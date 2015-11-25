94b32f40f1146f6154af219237f9b1ac394d0569
Morten Toudahl <toudahl@protonmail.ch>
2015-11-25 13:31:53 +0100

    ApiLink now authenticates with the api.
    Updated TeacherLoungeBroadcast to print out information.
    added try/catch to HandleBroadcast

--- ExamProject/SensorReader/ApiLink.cs
+++ ExamProject/SensorReader/ApiLink.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SensorReader
@@ -12,12 +13,66 @@ namespace SensorReader
     class ApiLink
     {
         private readonly string _apiPath;
-        private const string ServerUrl = "https://awesomeduckninjas.azurewebsites.net/api/";
+        private const string ServerUrl = "http://awesomeduckninjas.azurewebsites.net/api/";
+        private TokenResponce _token;
+        private bool hasToken;
 
         public ApiLink(string apiPath)
         {
+            hasToken = false;
             _apiPath = apiPath;
+            GetToken("toudahl@gmail.com","Password1234.");
         }
+
+        private async void GetToken(string userName, string password)
+        {
+            using (var client = new HttpClient())
+            {
+                var tokenUrl = ServerUrl.Replace("api/", "") + "token";
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
+                client.Timeout = new TimeSpan(0, 0, 30);
+
+                var result = await client.PostAsync(tokenUrl, new StringContent($"username={userName}&password={password}&grant_type=password"));
+                if (result.IsSuccessStatusCode)
+                {
+                    _token = await result.Content.ReadAsAsync<TokenResponce>();
+
+                   hasToken = true;
+                }
+            }
+        }
+
+        #region TokenResponce class
+        class TokenResponce
+        {
+
+            public string access_token
+            {
+                get;
+                set;
+            }
+
+            public string token_type
+            {
+                get;
+                set;
+            }
+
+            public int expires_in
+            {
+                get;
+                set;
+            }
+
+            public string userName
+            {
+                get;
+                set;
+            }
+
+        }
+        #endregion
+
 
         #region PostAsJsonAsync(byte array)
 
@@ -49,7 +104,14 @@ namespace SensorReader
         /// <returns>HttpClient</returns>
         private HttpClient GetClient()
         {
+            int count = 1;
+            while(_token == null)
+            {
+                Thread.Sleep(500);
+            }
             var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token.access_token);
+
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.Timeout = new TimeSpan(0, 0, 30);
             return client;
@@ -63,9 +125,9 @@ namespace SensorReader
         /// <returns>HttpStatusCode.NoContent</returns>
         private HttpResponseMessage FailedClient(Exception e)
         {
-            var response = new HttpResponseMessage();
+            var response = new HttpResponseMessage(HttpStatusCode.BadRequest);
             response.ReasonPhrase = e.Message;
-            return new HttpResponseMessage(HttpStatusCode.NoContent);
+            return response;
         }
         #endregion
     }
--- ExamProject/SensorReader/TeacherLoungeBroadcast.cs
+++ ExamProject/SensorReader/TeacherLoungeBroadcast.cs
@@ -1,4 +1,7 @@
-﻿using BroadcastReceiver;
+﻿using System;
+using System.Diagnostics;
+using BroadcastReceiver;
+using Microsoft.SqlServer.Server;
 
 namespace SensorReader
 {
@@ -12,7 +15,22 @@ namespace SensorReader
         }
         public async void HandleBroadcast(byte[] input)
         {
-            await link.PostAsJsonAsync(input);
+            try
+            {
+                var result = await link.PostAsJsonAsync(input);
+                if (result.IsSuccessStatusCode)
+                {
+                    Trace.TraceInformation("Successfully posted the data to the api");
+                }
+                else
+                    Trace.TraceWarning("Posting the data failed with the following status code: " + result.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                
+                Trace.TraceWarning("An exception of the type: " +ex.GetType() + "occured");
+                Trace.TraceWarning(ex.Message);
+            }
         }
     }
 }
